ba9d958646b5f570affc3460f7ac79ffa2b3341a
fielmente8777 <122347218+fielmente8777@users.noreply.github.com>
2026-01-21 13:15:54 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   // images: {
   //   domains: [
   //     "eazotel-client-images.s3.ap-south-1.amazonaws.com",
@@ -9,21 +9,25 @@ const nextConfig = {
   // },
   trailingSlash: true,
   images: {
-    unoptimized: true,
-    // remotePatterns: [
-    //   {
-    //     protocol: "https",
-    //     hostname: "eazotel-client-images.s3.ap-south-1.amazonaws.com",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
-    //   },
-    //   {
-    //     protocol: "https",
-    //     hostname: "cdn.builder.io",
-    //   },
-    // ],
+    // unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "eazotel-client-images.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "eazotel-client-images.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "eazotel-client-webp-image.s3.ap-south-1.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.builder.io",
+      },
+    ],
   },
 };
 
